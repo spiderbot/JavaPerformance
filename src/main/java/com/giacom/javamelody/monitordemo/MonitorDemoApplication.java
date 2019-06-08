147bf1c02f1247d3da7681ffbe73365afa9d86a1
Sandro <sandro.giacomozzi@fluig.com>
2019-06-08 16:02:23 -0300

    add javamelody dependency and controller

---
+++ src/main/java/com/giacom/javamelody/monitordemo/MonitorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.giacom.javamelody.monitordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giacom/javamelody/monitordemo/MonitorDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.giacom.javamelody.monitordemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MonitorDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
